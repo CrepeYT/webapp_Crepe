4430cd949ec8d5820156c000bd69153cc36454a3
yodsawat <yodsawat.ch@rmuti.ac.th>
2024-01-10 11:59:28 +0700

    weak-10

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using API.Entities;
 
 namespace api;
 
 [Table("Photos")]
 public class Photo
 {
+  public int AppUserID { get; set; }
+
   public int Id { get; set; }
   public string Url { get; set; }
   public string PublicId { get; set; }
